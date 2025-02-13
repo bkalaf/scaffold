a6fc8299d371722452269497e43bfb60d77ea998
Bobby Kalaf <bobby@outlook.com>
2025-02-13 13:01:49 -0800

    feat: update Controls movement

--- InsertForms-template.tsx
+++ InsertForms-template.tsx
@@ -1,14 +1,11 @@
-import { FieldValues } from 'react-hook-form';
-import { RowData } from '@tanstack/react-table';
-import { OIDElement } from '../Controls/OIDElement';
+import {FieldValues} from 'react-hook-form';
+import {RowData} from '@tanstack/react-table';
+import {OIDElement} from '../../components/Controls/OIDElement';
 
 export function InsertForm<T extends FieldValues & RowData>() {
     return (
         <>
-            <OIDElement
-                name='_id'
-                label='OID'
-            />
+            <OIDElement name='_id' label='OID' />
         </>
     );
 }
