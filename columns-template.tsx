a4c5baae9f834d25354d6ec4edaa4ab0ca66983c
Bobby Kalaf <bobby@outlook.com>
2025-02-13 12:59:09 -0800

    feat: update col movement

--- columns-template.tsx
+++ columns-template.tsx
@@ -1,6 +1,6 @@
 import {createColumnHelper, ColumnDef} from '@tanstack/react-table';
 import dayjs from 'dayjs';
-import {col} from '../col';
+import {col} from '../../util/col';
 import {I@@TYPENAME@@} from '../@@COLLECTIONNAME@@';
 
 const helper = createColumnHelper<I@@TYPENAME@@>();
